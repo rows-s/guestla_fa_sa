fe07a481bee49ae9455879edbf1133374ee4a748
vladimirrows <I3rovvser@gmail.com>
2022-03-20 14:40:46 +0000

    add UserCreate. Represents User structure for POST/PUT requests.

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,13 +1,21 @@
+from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
-__all__ = ('User', )
+__all__ = ('User', 'UserCreate')
 
 
 Base = declarative_base()
 
 
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+
 class User(Base):
+    __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String)
     password = Column(String)
