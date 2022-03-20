4d3c5a7284bc9023d3184396311fa8ef982d6201
vladimirrows <I3rovvser@gmail.com>
2022-03-20 15:36:05 +0000

    change db.config to db.url/dsn.

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,9 +1,3 @@
 from .models import *
 
-config = {  # TODO: use environment variables
-    'host': '127.0.0.1',
-    'port': 5432,
-    'database': 'test',
-    'user': 'testuser',
-    'password': 'TESTpassWORD'
-}
+url = 'postgresql://testuser:TESTpassW0RD@localhost/test'
