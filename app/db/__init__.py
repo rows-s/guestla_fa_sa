2e2b5d06d818a6efc33ece18a80e3a31b06b7177
vladimirrows <I3rovvser@gmail.com>
2022-03-22 17:13:51 +0000

    -db.make_async_session()

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,21 +1,2 @@
-from contextlib import asynccontextmanager
-
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-
 from .models import *
-
-url = 'postgresql+asyncpg://testuser:TESTpassW0RD@localhost/test'  # TODO: use env var
-engine = create_async_engine(url)
-
-
-@asynccontextmanager
-async def make_async_session(engine_=engine, **kw) -> AsyncSession:
-    session = sessionmaker(engine_, class_=AsyncSession, **kw)()  # called!
-    try:
-        yield session
-        # else block:
-        await session.commit()
-    finally:
-        await session.close()
-
+from .sessions import *
