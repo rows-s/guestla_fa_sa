e9b42d28c3f80099d1bca49299f4ead520ec4a92
vladimirrows <I3rovvser@gmail.com>
2022-03-22 17:08:10 +0000

    add Base model.

---
+++ app/db/models/base.py
@@ -0,0 +1,15 @@
+from sqlalchemy import inspect
+from sqlalchemy.orm import declarative_base
+
+
+__all__ = ['Base']
+
+sa_Base = declarative_base()
+
+
+class Base(sa_Base):
+    __abstract__ = True
+
+    def _asdict(self):
+        return {c.key: getattr(self, c.key)
+                for c in inspect(self).mapper.column_attrs}
