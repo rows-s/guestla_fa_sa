add53ae316cc1c1cf67c7493d3eba81c65edebb6
vladimirrows <I3rovvser@gmail.com>
2022-03-23 12:16:04 +0000

    db.AsyncSession(self, engine=base_engine, **kw) -> (session_maker=async_session_maker)

--- app/db/sessions.py
+++ app/db/sessions.py
@@ -3,23 +3,29 @@ from sqlalchemy.orm import sessionmaker
 
 from .engines import base_engine
 
-__all__ = ['AsyncSession']
+__all__ = ['AsyncSession', 'async_session_maker']
+
+
+async_session_maker = sessionmaker(base_engine, expire_on_commit=False)
 
 
 class AsyncSession:
     """
     Async context manager / Session container. Use only in `async with` statement.
 
+    Warnings:
+        expire_on_commit=False by default. The session is closed on exit the context manager.
+
     Examples:
         >>> async with AsyncSession() as session:
         >>>     pass
 
-        >>> session_container = AsyncSession()  # don't name it `session`, may confuse: `container`, `maker` whatever
+        >>> session_container = AsyncSession()  # don't name it `session`, may confuse. `container`, `maker` whatever
         >>> async with session_container as session:
         >>>     pass
     """
-    def __init__(self, engine=base_engine, **kw):
-        self.session: sa_AsyncSession = sessionmaker(engine, class_=sa_AsyncSession, **kw)()
+    def __init__(self, session_maker: sessionmaker = async_session_maker):
+        self.session: sa_AsyncSession = session_maker()
 
     async def __aenter__(self) -> sa_AsyncSession:
         return self.session
