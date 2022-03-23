0d1a8f2148c873b712ae1073919f6cfdeb445453
vladimirrows <I3rovvser@gmail.com>
2022-03-23 16:46:43 +0000

    +db.MobelDAL

--- app/db/models/base.py
+++ app/db/models/base.py
@@ -1,8 +1,11 @@
-from sqlalchemy import inspect
+from abc import ABC
+from typing import AsyncGenerator
+
+from sqlalchemy import inspect, select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import declarative_base
 
-
-__all__ = ['Base']
+__all__ = ['Base', 'ModelDAL']
 
 sa_Base = declarative_base()
 
@@ -13,3 +16,17 @@ class Base(sa_Base):
     def asdict(self):
         return {c.key: getattr(self, c.key)
                 for c in inspect(self).mapper.column_attrs}
+
+
+class ModelDAL(ABC):
+    def __init__(self, session: AsyncSession, model: Base):
+        self.session: AsyncSession = session
+        self.model: Base = model
+
+    async def objects(self) -> AsyncGenerator[Base, None]:
+        result = await self.session.stream(select(self.model))
+        async for obj in result.scalars():
+            yield obj
+
+
+
