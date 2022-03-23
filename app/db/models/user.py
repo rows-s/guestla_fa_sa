2936d8aeb06731a348327a41f997865fc90841e4
vladimirrows <I3rovvser@gmail.com>
2022-03-23 16:46:13 +0000

    models moduls will have structure: 1. Model 2. DAL 3. Create

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,19 +1,19 @@
 import pydantic
 from sqlalchemy import Column, Integer, String
 
-from .base import Base
+from . import base
 
 __all__ = ['User', 'UserCreate']
 
 
-class UserCreate(pydantic.BaseModel):
-    email: str
-    password: str
-
-
-class User(Base):
+class User(base.Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, unique=True)
     password = Column(String)
+
+
+class UserCreate(pydantic.BaseModel):
+    email: str
+    password: str
