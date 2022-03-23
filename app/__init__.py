41835f887d1a8c6d2c282381561e8db3a43dbfc7
vladimirrows <I3rovvser@gmail.com>
2022-03-23 12:16:46 +0000

    fix calls edited signatures.

--- app/__init__.py
+++ app/__init__.py
@@ -14,18 +14,21 @@ async def root():
 
 @app.get('/user/{user_id}')
 async def read_user(user_id: int):
-    async with db.AsyncSession(expire_on_commit=False) as session:
-        users = await session.execute(select(db.User).where(db.User.id == user_id))
-        user = users.scalar()
+    async with db.AsyncSession() as session:
+        result = await session.execute(
+            select(db.User).where(db.User.id == user_id)
+        )
+        user = result.scalar()
         if user is None:
             raise HTTPException(404, f'User with `id={user_id}` not found')
-    return user._asdict()
+        return user.asdict()
 
 
 @app.post('/user')
 async def post_user(user: db.UserCreate):
-    async with db.AsyncSession(expire_on_commit=False) as session:
+    async with db.AsyncSession() as session:
         user = db.User(**user.dict())
         session.add(user)
-    print(f"Added user {user.id}")
-    return {'id': user.id}
+        print(f"Added user {user.id}")
+        await session.commit()
+        return {'id': user.id}
