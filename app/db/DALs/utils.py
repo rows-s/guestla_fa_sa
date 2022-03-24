e71676c9bf66e0a51ab35af444c94ff6fb0a51f0
vladimirrows <I3rovvser@gmail.com>
2022-03-24 11:03:58 +0000

    db.utils.user_dal_maker() -> db.utils.get_user_dal() use FastAPI.Depends() in each route.

--- app/db/DALs/utils.py
+++ app/db/DALs/utils.py
@@ -1,10 +1,10 @@
 from contextlib import asynccontextmanager
-from typing import TypeVar, ContextManager
+from typing import TypeVar, ContextManager, AsyncGenerator
 
 from ..DALs import *
 from ..sessions import async_session_maker
 
-__all__ = ['dal_maker', 'user_dal_maker']
+__all__ = ['dal_maker', 'get_user_dal']
 _T = TypeVar('_T')
 
 
@@ -12,9 +12,9 @@ _T = TypeVar('_T')
 async def dal_maker(class_: _T) -> ContextManager[_T]:
     async with async_session_maker() as session:
         yield class_(session)
+        await session.commit()
 
 
-@asynccontextmanager
-async def user_dal_maker() -> ContextManager[UserDAL]:
+async def get_user_dal() -> AsyncGenerator[UserDAL, None]:
     async with dal_maker(UserDAL) as dal:
         yield dal
--- app/users.py
+++ app/users.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 
 from . import db
-from .db.utils import user_dal_maker
+from .db.utils import get_user_dal
 from .tags import Tags
 
 __all__ = ['router', 'tags']
@@ -11,23 +11,27 @@ tags = [Tags.users]
 
 
 @router.get('/')
-async def iter_users():
-    async with user_dal_maker() as user_dal:
-        return {'users': await (await user_dal.objects()).all()}
+async def iter_users(user_dal=Depends(get_user_dal)):
+    users_gen = user_dal.objects()
+    return {'users': await users_gen.all()}
 
 
 @router.get('/{user_id}')
-async def read_user(user_id: int):
-    async with user_dal_maker() as user_dal:
-        user = await user_dal.get(user_id)
-        if user is None:
-            raise HTTPException(404, f"user with `id={user_id}` doesn't exist")
-        return user.asdict()
+async def read_user(
+        user_id: int,
+        user_dal=Depends(get_user_dal)
+):
+    user = await user_dal.get(user_id)
+    if user is None:
+        raise HTTPException(404, f"user with `id={user_id}` doesn't exist")
+    return user.asdict()
 
 
 @router.post('/')
-async def post_user(user: db.UserCreate):
-    async with user_dal_maker() as user_dal:
-        user = await user_dal.create(**user.dict())
-    print(f"Added user {user.id}")
+async def post_user(
+        user: db.UserCreate,
+        user_dal: db.UserDAL = Depends(get_user_dal)
+):
+    user = await user_dal.create(**user.dict())
+    await user_dal.flush()
     return {'id': user.id}
