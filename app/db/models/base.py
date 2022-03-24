98de091e804d56809ac914592056a853a1a2072d
vladimirrows <I3rovvser@gmail.com>
2022-03-24 10:20:14 +0000

    +db.Base.pk property

--- app/db/models/base.py
+++ app/db/models/base.py
@@ -1,7 +1,9 @@
 from sqlalchemy import inspect
 from sqlalchemy.orm import declarative_base
 
+
 __all__ = ['Base']
+
 
 sa_Base = declarative_base()
 
@@ -13,6 +15,11 @@ class Base(sa_Base):
     def asdict(self):
         return {c.key: getattr(self, c.key)
                 for c in inspect(self).mapper.column_attrs}
+
+    @classmethod
+    @property
+    def pk(cls):
+        return inspect(cls).primary_key[0]
 
 
 
