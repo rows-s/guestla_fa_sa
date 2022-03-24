980ff7accb9a02caede4ff00c7d5ea843d893c6a
vladimirrows <I3rovvser@gmail.com>
2022-03-24 23:21:55 +0000

    rebuild tabel

--- alembic/versions/49cd567e1f9f_create_users_table.py
+++ alembic/versions/49cd567e1f9f_create_users_table.py
@@ -1,15 +1,15 @@
 """create users table
 
-Revision ID: 771f57a6336b
+Revision ID: 49cd567e1f9f
 Revises: 
-Create Date: 2022-03-21 10:07:16.101180
+Create Date: 2022-03-24 14:01:36.788963
 
 """
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '771f57a6336b'
+revision = '49cd567e1f9f'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -18,9 +18,9 @@ depends_on = None
 def upgrade():
     op.create_table(
         'users',
-        sa.Column('id', sa.Integer, primary_key=True, index=True),
-        sa.Column('email', sa.String),
-        sa.Column('password', sa.String),
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('email', sa.String, unique=True, nullable=False),
+        sa.Column('password', sa.String, nullable=False),
     )
 
 
