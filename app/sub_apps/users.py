492e5cad2e9839125438e592ccc80afba8f68000
vladimirrows <I3rovvser@gmail.com>
2022-03-25 13:36:41 +0000

    add page_size for `users`.

--- app/sub_apps/users.py
+++ app/sub_apps/users.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Query
 from sqlalchemy.exc import IntegrityError
 
-from . import db
-from .db.utils import get_user_dal
-from .tags import Tags
+from app import db
+from app.db.utils import get_user_dal
+from app.sub_apps.config import Tags
 
 __all__ = ['router', 'tags']
 
@@ -11,16 +11,20 @@ router = APIRouter()
 tags = [Tags.users]
 
 
-@router.get('/')
-async def iter_users(user_dal=Depends(get_user_dal)):
+@router.get('')
+async def read_users(
+        page_size: int = Query(100, le=100),
+        user_dal: db.UserDAL = Depends(get_user_dal)
+):
     users_gen = await user_dal.objects()
-    return {'data': {'users': await users_gen.all()}}
+    async for page in users_gen.partitions(page_size):
+        return {'data': {'users': page}}
 
 
 @router.get('/{user_id}')
 async def read_user(
         user_id: int,
-        user_dal=Depends(get_user_dal)
+        user_dal: db.UserDAL = Depends(get_user_dal)
 ):
     user = await user_dal.get(user_id)
     if user is None:
@@ -28,14 +32,14 @@ async def read_user(
     return {'data': {'user': user.asdict()}}
 
 
-@router.post('/')
+@router.post('')
 async def post_user(
         user: db.UserCreate,
         user_dal: db.UserDAL = Depends(get_user_dal)
 ):
     try:
         user = await user_dal.create(**user.dict(), shld_flush=True)
-    except IntegrityError:
+    except IntegrityError:  # TODO: Must check which column is duplicated
         raise HTTPException(409, f'user with `email={user.email}` already exist')
     else:
         return {'data': {'id': user.id}}
