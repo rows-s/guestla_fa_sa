365e950f77ea67e699eae300a601e883d1af9160
vladimirrows <I3rovvser@gmail.com>
2022-03-25 13:38:02 +0000

    use `sub_apps` package for `users` sub app and coming others.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from . import db
-from . import users
+from .sub_apps import users
 
 app = FastAPI()
 app.include_router(users.router, prefix='/users', tags=users.tags)
