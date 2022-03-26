c44f8629756d27a9849ba9b88cae82c4c505d874
vladimirrows <I3rovvser@gmail.com>
2022-03-26 16:20:50 +0000

    db.utils extended with db.models.utils;

--- app/db/utils.py
+++ app/db/utils.py
@@ -1,5 +1,8 @@
 from .DALs import utils as dals_utils
 from .DALs.utils import *  # noqa
+from .models import utils as models_utils
+from .models.utils import * # noqa
 
 __all__ = []
 __all__.extend(dals_utils.__all__)
+__all__.extend(models_utils.__all__)
