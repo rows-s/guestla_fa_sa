e02c052c4557e506922b6c99e4611a866dbd4bbd
vladimirrows <I3rovvser@gmail.com>
2022-03-26 16:21:55 +0000

    -db.utils.get_user_dal()

--- app/db/DALs/utils.py
+++ app/db/DALs/utils.py
@@ -1,10 +1,9 @@
 from contextlib import asynccontextmanager
-from typing import TypeVar, AsyncGenerator, AsyncContextManager, Type
+from typing import TypeVar, AsyncContextManager, Type
 
-from ..DALs import *
 from ..sessions import async_session_maker
 
-__all__ = ['dal_maker', 'get_user_dal']
+__all__ = ['dal_maker']
 _T = TypeVar('_T')
 
 
@@ -13,8 +12,3 @@ async def dal_maker(class_: Type[_T]) -> AsyncContextManager[_T]:
     async with async_session_maker() as session:
         yield class_(session)
         await session.commit()
-
-
-async def get_user_dal() -> AsyncGenerator[UserDAL, None]:
-    async with dal_maker(UserDAL) as dal:
-        yield dal
